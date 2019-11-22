30ebd79f2e60d4da6c18951c3e1e55d26cc788a6
Nguyễn Trung Hiếu <trunghieu99tt@users.noreply.github.com>
2019-11-22 23:30:28 +0700

    Updates

---
+++ precache-manifest.86d04be636272f07c298e627361301d5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "373e7da13b2ac6ba1d4d8f8e40e28d5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee4c45777da08b667619",
+    "url": "/static/css/main.59f090ef.chunk.css"
+  },
+  {
+    "revision": "c2cad22d10c3a4cb428c",
+    "url": "/static/js/2.192c63f6.chunk.js"
+  },
+  {
+    "revision": "ee4c45777da08b667619",
+    "url": "/static/js/main.208417ec.chunk.js"
+  },
+  {
+    "revision": "fde51bea53981dd85127",
+    "url": "/static/js/runtime-main.65ac8c94.js"
+  },
+  {
+    "revision": "c509ae1bf8d7f28a998724af6eba339e",
+    "url": "/static/media/coffee_logo_4.c509ae1b.png"
+  },
+  {
+    "revision": "b0df04a4a9af8cd493da2af726e26a88",
+    "url": "/static/media/introImg.b0df04a4.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "654727d7733ef13fac1ffe911dc19b3d",
+    "url": "/static/media/slide_img_1.654727d7.jpg"
+  },
+  {
+    "revision": "b29947a115ef644c8898511a185639d2",
+    "url": "/static/media/slide_img_2.b29947a1.jpg"
+  },
+  {
+    "revision": "c14d69b5794dc709a625107f0d9a5d7a",
+    "url": "/static/media/slide_img_3.c14d69b5.jpg"
+  },
+  {
+    "revision": "55dc1f644dfbb79329a049f41518a903",
+    "url": "/static/media/slide_img_4.55dc1f64.jpg"
+  }
+]);
